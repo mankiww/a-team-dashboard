c2201dbb2aef5ffa92dd8677d99a8411beea8f02
ParkJeongseon <wjdtjs529@gmail.com>
2022-03-01 15:26:03 +0900

    Add: Initial Setting

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = (_, argv) => ({
+  entry: {
+    "js/app": ["./src/main.tsx"],
+  },
+  output: {
+    path: path.join(__dirname, "dist/"),
+    publicPath: argv.mode === "development" ? "/" : "./",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+});
