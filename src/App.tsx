0231ca4b979aee8d9b7c3fdbe813753819c0902e
ParkJeongseon <wjdtjs529@gmail.com>
2022-03-01 18:54:10 +0900

    Add: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,9 @@
 import styled from "styled-components";
-import { MdDomain } from "react-icons/md";
 
-import logo from "./image/logo.png";
 import GlobalStyle from "./GlobalStyle";
 
+import Header from "./components/Header";
 import CardList from "./components/CardList";
-
-const Header = styled.header`
-  position: absolute;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  bottom: 0%;
-  height: 70px;
-  color: #ffffff;
-  background: #1565C0;
-  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.12), 0px 2px 2px rgba(0, 0, 0, 0.24);
-`;
-
-const Image = styled.img`
-  position: absolute;
-  left: 2.78%;
-  right: 86.6%;
-  top: 35.71%;
-  bottom: 35.71%;
-`;
-
-const LoginInfo = styled.div`
-  position: absolute;
-  right: 40px;
-  top: 25px;
-
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  width: 208.67px;
-  height: 20px;
-  padding: 0px;
-
-  color: #FFFFFF;
-`;
-
-const UserInfo = styled.div`
-  display: flex;
-  align-items: center;
-  width: 105.67px;
-  height: 20px;
-
-  svg {
-    margin: 0 5px;
-  }
-`;
-
-const UserName = styled.div`
-  text-align: end;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 20px;
-  white-space: nowrap;
-`;
-
-const Divider = styled.div`
-  height: 10px;
-  border: 1px solid #FFFFFF;
-  margin: 0px 32px;
-`;
-
-const Button = styled.div`
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 20px;
-
-  display: flex;
-  align-items: center;
-  white-space: nowrap;
-
-  color: #FFFFFF;
-`;
 
 const Main = styled.div`
   padding: 155px;
@@ -172,17 +97,7 @@ export default function App() {
   return (
     <div>
       <GlobalStyle />
-      <Header>
-        <Image src={logo} alt="CAPA파트너스" />
-        <LoginInfo>
-          <UserInfo>
-            <MdDomain />
-            <UserName>A 가공 업체</UserName>
-          </UserInfo>
-          <Divider />
-          <Button>로그아웃</Button>
-        </LoginInfo>
-      </Header>
+      <Header />
       <Main>
         <Heading>
           <Title>들어온 요청</Title>
